e8972ceb8148ab5b050e257c4296cefc1d9bbd4d
Troy Maurer <troytmaurer@outlook.com>
2026-01-24 12:58:55 -0500

    1/24/2026: Outline for the IntakeSubsystem.

--- RebuiltComp/src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
+++ RebuiltComp/src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
@@ -1,7 +1,38 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakeSubsystem extends SubsystemBase {
-    
+    public static final IntakeSubsystem kIntake = new IntakeSubsystem();
+
+    enum IntakeMode{
+        STORED,
+        INTAKE,
+        OUTTAKE
+    }
+    private IntakeMode kMode;
+
+    private boolean kIsAtState;
+
+    private TalonFX kIntakeMotor;
+    private TalonFX kIntakeSlider;
+    private PIDController kSliderPID;
+
+    private IntakeSubsystem(){
+
+    }
+
+    private void moveToState(){}
+
+    public void setIntake(IntakeMode mNewMode){}
+    public IntakeMode getMode(){}
+    public boolean isReady(){}
+    public double getIntakeSpeed(){}
+
+    @Override
+    public void periodic(){}
 }
