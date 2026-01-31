9f5102291113beeeb1ba7838ea855de68ab13df7
Victor Lizarazu <victorlizarazu1@gmail.com>
2026-01-31 14:36:52 -0500

    1/31/2026 Added SpindexerMode functionality

--- RebuiltComp/src/main/java/frc/robot/Subsystems/SpindexerSubsystem.java
+++ RebuiltComp/src/main/java/frc/robot/Subsystems/SpindexerSubsystem.java
@@ -6,16 +6,22 @@ public class SpindexerSubsystem extends SubsystemBase {
     public static final SpindexerSubsystem kInstance = new SpindexerSubsystem();
 
     public enum SpindexerMode{
-
+        OFF,
+        LOAD
     }
     private SpindexerMode kMode;
 
     private SpindexerSubsystem(){
-
+        kMode = SpindexerMode.OFF;
     }
 
     private void moveSpindexer(){}
     public double getSpinSpeed(){}
-    public SpindexerMode getMode(){}
-    public void setMode(SpindexerMode mNewMode){}
+    public SpindexerMode getMode(){
+        return kMode;
+    }
+    public void setMode(SpindexerMode mNewMode){
+        kMode = mNewMode;
+        moveSpindexer();
+    }
 }
