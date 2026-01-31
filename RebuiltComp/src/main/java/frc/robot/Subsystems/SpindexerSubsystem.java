098c72b04b7d0bd8db0ec3cca09d6cb98611ffab
pizzan201966-afk <pizzan201966@gmail.com>
2026-01-31 14:27:11 -0500

    1/31/2026. Added motors to Spindexter

--- RebuiltComp/src/main/java/frc/robot/Subsystems/SpindexerSubsystem.java
+++ RebuiltComp/src/main/java/frc/robot/Subsystems/SpindexerSubsystem.java
@@ -1,5 +1,7 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SpindexerSubsystem extends SubsystemBase {
@@ -8,14 +10,19 @@ public class SpindexerSubsystem extends SubsystemBase {
     public enum SpindexerMode{
 
     }
+    private TalonFX kSpindexterMotor;
+    private TalonFX kToFlywheelMoter;
     private SpindexerMode kMode;
 
     private SpindexerSubsystem(){
-
+        kSpindexterMotor = new TalonFX(7);
+        kToFlywheelMoter = new TalonFX(6);
     }
 
     private void moveSpindexer(){}
-    public double getSpinSpeed(){}
+    public double getSpinSpeed(){
+        return kSpindexterMotor.get();
+    }
     public SpindexerMode getMode(){}
     public void setMode(SpindexerMode mNewMode){}
 }
