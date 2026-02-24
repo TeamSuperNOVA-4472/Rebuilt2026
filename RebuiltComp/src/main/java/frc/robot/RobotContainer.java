d335fab7b6f811bc8ef9a68d20ad4f7d48137d80
Troy Maurer <troytmaurer@outlook.com>
2026-02-24 16:59:13 -0500

    2/24/2026: Began integrating IntakeSubsystem.

--- RebuiltComp/src/main/java/frc/robot/RobotContainer.java
+++ RebuiltComp/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Commands.moveTurretAbsolute;
 import frc.robot.Commands.setIntake;
@@ -21,28 +22,24 @@ import frc.robot.Subsystems.SpindexerSubsystem.SpindexerMode;
 public class RobotContainer {
   IntakeSubsystem mIntake;
   SpindexerSubsystem mSpindexer;
-  XboxController mDriver;
+  CommandXboxController mDriver;
   TurretSubsystem mTurret;
   SwerveSubsystem mSwerve;
   public RobotContainer() {
     mIntake = IntakeSubsystem.kIntake;
     mSpindexer = SpindexerSubsystem.kInstance;
-    mDriver = new XboxController(0);
+    mDriver = new CommandXboxController(0);
     mTurret = TurretSubsystem.kTurret;
     mSwerve = new SwerveSubsystem();
     configureBindings();
   }
 
   private void configureBindings() {
-    Trigger intakeToggle = new Trigger(mDriver::getLeftBumperButtonPressed);
-    intakeToggle.onTrue(new setIntake(mIntake, IntakeMode.INTAKE));
-    Trigger outtakeToggle = new Trigger(mDriver::getRightBumperButton);
-    outtakeToggle.onTrue(new setIntake(mIntake, IntakeMode.OUTTAKE));
-    Trigger spindexerHold = new Trigger(mDriver::getAButton);
-    spindexerHold.onTrue(new setSpindexer(mSpindexer, SpindexerMode.LOAD));
-    spindexerHold.onFalse(new setSpindexer(mSpindexer, SpindexerMode.OFF));
-    Trigger TurretTest = new Trigger(mDriver::getBButton);
-    TurretTest.onTrue(new moveTurretAbsolute(mTurret, mSwerve, 90));
+    mDriver.leftBumper().onTrue(new setIntake(mIntake, IntakeMode.INTAKE));
+    mDriver.rightBumper().onTrue(new setIntake(mIntake, IntakeMode.OUTTAKE));
+    mDriver.a().onTrue(new setSpindexer(mSpindexer, SpindexerMode.LOAD));
+    mDriver.a().onFalse(new setSpindexer(mSpindexer, SpindexerMode.OFF));
+    mDriver.b().onTrue(new moveTurretAbsolute(mTurret, mSwerve, 90));
   }
 
   public Command getAutonomousCommand() {
--- RebuiltComp/src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
+++ RebuiltComp/src/main/java/frc/robot/Subsystems/IntakeSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot.Subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.networktables.NetworkTableEvent.Kind;
@@ -14,6 +15,7 @@ import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Robot;
 
 public class IntakeSubsystem extends SubsystemBase {
@@ -43,33 +45,33 @@ public class IntakeSubsystem extends SubsystemBase {
     private IntakeSubsystem(){
         kMode = IntakeMode.STORED;
         kIsAtState = true;
-        kIntakeMotor = new TalonFX(IntakeSubsystemConstants.kIntakeMotorPort);
-        kIntakeSlider = new TalonFX(IntakeSubsystemConstants.kSliderMotorPort);
-        kSliderPID = new PIDController(IntakeSubsystemConstants.kSliderP,IntakeSubsystemConstants.kSliderI,IntakeSubsystemConstants.kSliderD);
-        kSliderPID.setTolerance(IntakeSubsystemConstants.kSlideThreshold,IntakeSubsystemConstants.kSlideSpeedThreshold);
+        kIntakeMotor = new TalonFX(Constants.IntakeSubsystemConstants.kIntakeMotorPort);
+        kIntakeSlider = new TalonFX(Constants.IntakeSubsystemConstants.kSliderMotorPort);
+        kSliderPID = new PIDController(Constants.IntakeSubsystemConstants.kSliderP, Constants.IntakeSubsystemConstants.kSliderI, Constants.IntakeSubsystemConstants.kSliderD);
+        kSliderPID.setTolerance(Constants.IntakeSubsystemConstants.kSlideThreshold, Constants.IntakeSubsystemConstants.kSlideSpeedThreshold);
         kIntakeSimMotor = DCMotor.getKrakenX60(1);
-        kIntakeSim = new ElevatorSim(kIntakeSimMotor, 5, 2.26796, 0.01524, 0, 0.4572, false, 0, 0, 0);
+        kIntakeSim = new ElevatorSim(kIntakeSimMotor, Constants.IntakeSubsystemConstants.kGearing, Constants.IntakeSubsystemConstants.kMass, Constants.IntakeSubsystemConstants.kDrumRadius, 0, Constants.IntakeSubsystemConstants.kMaxLen, false, 0, 0, 0);
         kSimSpace = new Mechanism2d(60, 60);
         kSimRoot = kSimSpace.getRoot("base", 10, 30);
-        kSimDisp = kSimRoot.append(new MechanismLigament2d("Intake", kIntakeSim.getPositionMeters()*39.3701*3, IntakeSubsystemConstants.kIntakeAngle));
+        kSimDisp = kSimRoot.append(new MechanismLigament2d("Intake", kIntakeSim.getPositionMeters()*Constants.IntakeSubsystemConstants.kSimLenMult, Constants.IntakeSubsystemConstants.kIntakeAngle));
         SmartDashboard.putData("IntakeSim", kSimSpace);
     }
 
     private void moveToState(){
         switch (kMode){
         case STORED:
-            kSliderTarget = IntakeSubsystemConstants.kStoredPos;
+            kSliderTarget = Constants.IntakeSubsystemConstants.kStoredPos;
             kIntakeMotor.set(0);
             break;
         
         case INTAKE:
-            kSliderTarget = IntakeSubsystemConstants.kOutPos;
-            kIntakeMotor.set(IntakeSubsystemConstants.kIntakeMotorSpeed);
+            kSliderTarget = Constants.IntakeSubsystemConstants.kOutPos;
+            kIntakeMotor.set(Constants.IntakeSubsystemConstants.kIntakeMotorSpeed);
             break;
         
         case OUTTAKE:
-            kSliderTarget = IntakeSubsystemConstants.kOutPos;
-            kIntakeMotor.set(-IntakeSubsystemConstants.kIntakeMotorSpeed);
+            kSliderTarget = Constants.IntakeSubsystemConstants.kOutPos;
+            kIntakeMotor.set(-Constants.IntakeSubsystemConstants.kIntakeMotorSpeed);
             break;
         
         }
@@ -93,9 +95,10 @@ public class IntakeSubsystem extends SubsystemBase {
     public void periodic(){
         kIsAtState = kSliderPID.atSetpoint();
         if (Robot.isReal()){
-            PIDOutput = kSliderPID.calculate(kIntakeSlider.getPosition().getValueAsDouble(),kSliderTarget);
+            //TODO: Fix kEncoderToInchesMult BEFORE TESTING
+            PIDOutput = MathUtil.clamp(kSliderPID.calculate(kIntakeSlider.getPosition().getValueAsDouble()*Constants.IntakeSubsystemConstants.kEncoderToInchesMult,kSliderTarget), -1, 1);
         } else{
-            PIDOutput = kSliderPID.calculate(kIntakeSim.getPositionMeters()*39.3701,kSliderTarget);
+            PIDOutput = MathUtil.clamp(kSliderPID.calculate(kIntakeSim.getPositionMeters()*39.3701,kSliderTarget), -1, 1);
         }
         kIntakeSlider.set(PIDOutput);
         SmartDashboard.putNumber("Current Mode", kMode.ordinal());
@@ -107,7 +110,7 @@ public class IntakeSubsystem extends SubsystemBase {
 
         kIntakeSim.update(0.02);
 
-        kSimDisp.setLength(kIntakeSim.getPositionMeters()*39.3701*3);
+        kSimDisp.setLength(kIntakeSim.getPositionMeters()*Constants.IntakeSubsystemConstants.kSimLenMult);
         SmartDashboard.putNumber("Intake Length Horizontal", kIntakeSim.getPositionMeters()*39.3701*Math.cos(Math.toRadians(15)));
     }
 }
