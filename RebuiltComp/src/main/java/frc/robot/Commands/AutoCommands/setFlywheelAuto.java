5c27633223893129daa60eaf237cd2264cb0e430
Victor Lizarazu <victorlizarazu1@gmail.com>
2026-03-18 18:07:48 -0400

    3/18/26 Fixed setflywheel auto problems

--- RebuiltComp/src/main/java/frc/robot/Commands/AutoCommands/setFlywheelAuto.java
+++ RebuiltComp/src/main/java/frc/robot/Commands/AutoCommands/setFlywheelAuto.java
@@ -12,12 +12,10 @@ import frc.robot.Subsystems.FlywheelSubsystem.FlywheelMode;
 public class setFlywheelAuto extends InstantCommand {
     private final FlywheelSubsystem kFlywheel;
     private final double kDistance;
-    private final boolean kIsScoring;
 
-    public setFlywheelAuto(FlywheelSubsystem mFlywheelSubsystem, double mDistance, boolean mIsScoring){
+    public setFlywheelAuto(FlywheelSubsystem mFlywheelSubsystem, double mDistance){
         kFlywheel = mFlywheelSubsystem;
         kDistance = mDistance;
-        kIsScoring = mIsScoring;
 
         addRequirements(kFlywheel);
     }
@@ -27,14 +25,11 @@ public class setFlywheelAuto extends InstantCommand {
         double angle;
         double speed;
         double distance = kDistance;
-        if (kIsScoring)
-        {
-            if (distance >= FlywheelConstants.kDistanceThresholdInMeters && distance <= FlywheelConstants.kDistanceMaximumInMeters){
+            if (distance >= FlywheelConstants.kDistanceMinimumInMeters && distance <= FlywheelConstants.kDistanceMaximumInMeters){
                 speed = FlywheelConstants.kDistanceToFlywheelSpeed.get(distance);
                 angle = FlywheelConstants.kDistanceToHoodAngle.get(distance);
                 kFlywheel.setHoodTarget(angle);
                 kFlywheel.setMode(FlywheelMode.SPINNING, speed);    
             }
-        }
     }
 }
